eb1e677ed5f12403f174d822a7145748a2dec729
ilikemakingmaps <140996722+ilikemakingmaps@users.noreply.github.com>
2023-07-30 20:37:42 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @ilikemakingmaps
+- 👀 I’m interested in helping people with sideprojects
+- 🌱 I’m currently learning to code
+- 💞️ I’m looking to collaborate on projects
+- 📫 How to reach me steam
+
+<!---
+ilikemakingmaps/ilikemakingmaps is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
